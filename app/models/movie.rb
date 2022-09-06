c2c3dec1645342769358a93f488c2e13cf32d937
Tyler <toygrrr@gmail.com>
2022-09-06 13:44:25 -0700

    commit tree 215aa3b1b97a2ae77b9d5d2c982d7a13fe86457c

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title title
+      Movie.create(title: "#{title}")
+    end
+   
+    def self.first_movie
+        Movie.find_by(id:first)
+    end
+    
+    #lucky guess? how does this work?
+    def self.last_movie
+    Movie.find_by(id:last)
+    end
+    
+    def self.movie_count
+    Movie.count
+    end
+    
+    def self.find_movie_with_id id
+    Movie.find_by(id:id)
+    end
 
+    def self.find_movie_with_attributes attrib
+    Movie.find_by(attrib)
+    end
+
+    def self.find_movies_after_2002
+    Movie.where("release_date > ?", 2002)
+    end
+
+    def update_with_attributes attribute
+    self.update(attribute)
+    end
+
+    def self.update_all_titles value
+    Movie.update({title: "#{value}"})
+    end
+
+    def self.delete_by_id id
+    Movie.destroy(id)
+    end
+
+    def self.delete_all_movies
+    Movie.destroy_all
+    end
 end
